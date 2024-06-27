da19ec49f01d0c8d41d19e0072fc59887d07b07a
Granu-Dragos-Vlad <92181965+Granu-Dragos-Vlad@users.noreply.github.com>
2024-06-27 21:58:53 +0300

    Update

--- AgentLocatie.java
+++ AgentLocatie.java
@@ -10,17 +10,16 @@ public class AgentLocatie extends Agent {
 	protected void setup() {
 		System.out.println("AgentLocatie " + getAID().getName() + " este activ.");
 
-		// Comportamentul agentului pentru a primi solicitări de la agenții de livrare
 		addBehaviour(new CyclicBehaviour(this) {
 			public void action() {
 				MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
 				ACLMessage msg = receive(mt);
 				if (msg != null) {
-					// Procesează cererea de la agentul de livrare
+					
 					String content = msg.getContent();
 					System.out.println("AgentLocatie " + getAID().getName() + " a primit o cerere: " + content);
 
-					// Exemplu de răspuns cu informații despre locație
+					
 					ACLMessage reply = msg.createReply();
 					reply.setPerformative(ACLMessage.INFORM);
 					reply.setContent("Mesajul trimis este " + content);
